f86105e491c5a153d1434a260e61ded8b18fb972
Asildar-Hajiyev <tu201806131@code.edu.az>
2022-03-11 14:03:46 +0400

    asasd

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Motor = 2.3;
+            car.DriveWay = 5;
+            car.DriveTime = 23;
+            car.Cycle = "123";
+            car.Color = "Black";
+            Console.WriteLine("Car; " + car.Motor + " Motor; " + car.DriveWay + " DriveWay; " + car.DriveTime + " DriweTime; " + car.Cycle + " Cycle; " + car.Color + " Color; ");
+            Console.WriteLine();
+            Bicycle bicycle = new Bicycle();
+            bicycle.DriveWay = 95;
+            bicycle.DriveTime = 17;
+            bicycle.Cycle = "3456";
+            bicycle.Color = "Black";
+            Console.WriteLine("Bicycle; " +  bicycle.DriveWay + " DriveWay; " + bicycle.DriveTime + " DriweTime; " + bicycle.Cycle + " Cycle; " + bicycle.Color + " Color; ");
+            Console.WriteLine();
+            Bus bus = new Bus();
+            bus.DriveWay = 225;
+            bus.DriveTime = 23;
+            bus.Cycle = "30200";
+            bus.Color = "Yellow";
+            Console.WriteLine("Bus; " + " Motor; " + bus.DriveWay + " DriveWay; " + bus.DriveTime + " DriweTime; " + bus.Cycle + " Cycle; " + bus.Color + " Color; ");
+
+
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkAround
+{
+  public  class Vehicle
+    {
+        public string Cycle { get; set; }
+        public double Motor { get; set; }
+        public int DriveWay { get; set; }
+        public string Color { get; set; }
+        public int PassengerCount { get; set; }
+        public int DriveTime { get; set; }
+
+        public int GetSpeed(int getspeed)
+        {
+            return getspeed;
+        }
+    }
+}
